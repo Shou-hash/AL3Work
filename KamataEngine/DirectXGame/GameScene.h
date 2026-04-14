6da635a6b66ef327ee0be06077f20e0c2a7c027e
Shou <k025g0060@g.neec.ac.jp>
2026-04-15 01:17:05 +0900

    基盤

---
+++ KamataEngine/DirectXGame/GameScene.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Kamataengine.h"
+
+class GameScene 
+{
+public:
+	//初期化
+	void Initialize();
+
+	//更新
+	void Update();
+
+	// 描画
+	void Draw();
+};
--- KamataEngine/DirectXGame/main.cpp
+++ KamataEngine/DirectXGame/main.cpp
@@ -1,7 +1,48 @@
 #include <Windows.h>
+#include"Kamataengine.h"
+#include"GameScene.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	//初期化
+	KamataEngine::Initialize(L"LE2A_12_ショウ_ズーウェン_AL3");
+
+	using namespace KamataEngine;
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	GameScene* gameScene = new GameScene();
+	gameScene->Initialize();
+
+	//メインループ
+	while (true) 
+	{
+		//エンジンの更新
+		if (KamataEngine::Update()) 
+		{
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画前処理
+		dxCommon->PreDraw();
+
+		// 描画処理
+		gameScene->Draw();
+
+		// 描画後処理
+		dxCommon->PostDraw();
+	}
+
+	//エンジンの終了処理
+	KamataEngine::Finalize();
+
+	//開放処理
+	delete gameScene;
+	gameScene = nullptr;
+
 	return 0;
 }
