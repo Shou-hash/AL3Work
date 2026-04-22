c2d251e3eb49a18813a7fb9e437642d75759fd39
Shou <k025g0060@g.neec.ac.jp>
2026-04-22 14:12:07 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -17,9 +17,12 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	gameScene->Initialize();
 
 	// メインループ
-	while (true) {
+	while (true) 
+	{
+
 		// エンジンの更新
-		if (KamataEngine::Update()) {
+		if (KamataEngine::Update()) 
+		{
 			break;
 		}
 		// ImGui受付開始
