95d48fbf3e9365f72401a107e8370252b581249d
Shou <k025g0060@g.neec.ac.jp>
2026-05-27 14:48:26 +0900

    最新版

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -25,9 +25,7 @@ void GameScene::Initialize() {
 
 	GenerateBlocks();
 
-	model_ = Model::CreateFromOBJ("cube", true);
-
-	player_->SetMapChipField(mapChipField_);
+	model_ = Model::CreateFromOBJ("block", true);
 
 	worldTransform_.Initialize();
 	camera_.Initialize();
@@ -48,7 +46,7 @@ void GameScene::Initialize() {
 	cameraController_->Initialize(&camera_);
 	cameraController_->SetTarget(player_.get());
 
-	Rect stageArea = {10.0f, 100.0f, 5.0f, 200.0f};
+	Rect stageArea = {10.0f, 90.0f, 5.0f, 100.0f};
 	cameraController_->SetMovableArea(stageArea);
 
 	cameraController_->Reset();
