a2b0163373574487584ca1ff981e8e4c3f5f1239
Shou <k025g0060@g.neec.ac.jp>
2026-07-08 14:44:47 +0900

    AL3_05_13

---
+++ DirectXGame/Fade.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <KamataEngine.h>
+
+class Fade {
+
+public:
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+private:
+
+	KamataEngine::Sprite* sprite_ = nullptr;
+
+};
